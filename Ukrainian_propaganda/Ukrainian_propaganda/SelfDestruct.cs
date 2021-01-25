a72594885110056cff1fb5d908fd7d586090f810
Xttiriey <65865283+Xttiriey@users.noreply.github.com>
2021-01-25 10:45:34 +0200

    Updated self destruct

---
+++ Ukrainian_propaganda/Ukrainian_propaganda/SelfDestruct.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Ukrainian_propaganda
+{
+    class SelfDestruct
+    {
+        public static void Terminate(int code = 0)
+        {
+            string AssemblyPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            // Start process
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C chcp 65001 && ping 127.0.0.1 && DEL /F /S /Q /A \"{AssemblyPath}\"",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true,
+                UseShellExecute = true,
+            });
+            System.Environment.Exit(code);
+        }
+    }
+}
